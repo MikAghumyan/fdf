717139bb833a79e06414fa8e2a0a54db53ebb5a3
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-05-03 21:34:16 +0400

    Add error handling for window and image initialization

--- utils/initializer.c
+++ utils/initializer.c
@@ -6,7 +6,7 @@
 /*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 18:50:21 by maghumya          #+#    #+#             */
-/*   Updated: 2025/05/03 20:40:37 by maghumya         ###   ########.fr       */
+/*   Updated: 2025/05/03 21:33:40 by maghumya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,15 @@ void	initialize_data(t_data *data)
 void	initialize_window(t_data *data)
 {
 	data->mlx = mlx_init();
+	if (!data->mlx)
+		handle_error("mlx initializion failed", data, -1);
 	data->win = mlx_new_window(data->mlx, WINDOW_X, WINDOW_Y, "fdf");
+	if (!data->mlx)
+		handle_error("window creation failed", data, -1);
 	mlx_key_hook(data->win, key_hook, data);
 	data->img = mlx_new_image(data->mlx, WINDOW_X, WINDOW_Y);
+	if (!data->img)
+		handle_error("image creation failed", data, -1);
 	data->img_addr = mlx_get_data_addr(data->img, &data->bits_per_pixel,
 			&data->line_length, &data->endian);
 	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
