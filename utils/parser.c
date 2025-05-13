e9f4132c231f55744af7442e0e88a7e5638d07a6
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-05-14 00:50:11 +0400

    fix z_min and z_max

--- utils/parser.c
+++ utils/parser.c
@@ -6,7 +6,7 @@
 /*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:47:37 by maghumya          #+#    #+#             */
-/*   Updated: 2025/05/14 00:17:12 by maghumya         ###   ########.fr       */
+/*   Updated: 2025/05/14 00:48:41 by maghumya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,19 @@ static ssize_t	fill_point(t_data *data, int row_i, int col_i, char **nbr_strs)
 	if (!point_data)
 		return (-1);
 	data->matrix[row_i][col_i] = ft_atoi(point_data[0]);
+	if (data->matrix[row_i][col_i] < data->z_min)
+		data->z_min = data->matrix[row_i][col_i];
+	else if (data->matrix[row_i][col_i] > data->z_max)
+		data->z_max = data->matrix[row_i][col_i];
 	if (point_data[1])
 	{
 		data->colors[row_i][col_i] = ft_atoi_base(point_data[1] + 2, 16);
-		if (data->colors[row_i][col_i] < data->z_min)
-			data->z_min = data->colors[row_i][col_i];
-		else if (data->colors[row_i][col_i] > data->z_max)
-			data->z_max = data->colors[row_i][col_i];
 	}
 	else
 		data->colors[row_i][col_i] = data->def_color
 			+ data->matrix[row_i][col_i] * 500;
 	free_matrix((void **)point_data);
+	printf("z_min: %d \t z_max: %d", data->z_min, data->z_max);
 	return (0);
 }
 
